5d400064183eb7aae39748084434e459a72274ba
Matthew Hunger <matt.hunger@gmail.com>
2017-09-04 12:27:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ var katzDeli = [];
 
 function takeANumber(otherDeli, person) {
   otherDeli.push(person);
-
-  return (`Welcome ${person}, you are number ${katzDeliLine[person]} in line.`)
+  return (`Welcome ${person}, you are number ${otherDeli[person]} in line.`)
 }
