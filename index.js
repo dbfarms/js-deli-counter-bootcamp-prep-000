7d023e5f9eb89b794c43d23ae8ca070cdbec5716
Matthew Hunger <matt.hunger@gmail.com>
2017-09-04 13:52:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,9 +18,10 @@ function nowServing(katzDeli) {
 }
 
 function currentLine(katzDeli) {
+  var otherDeli = katzDeli;
   if (katzDeli.length === 0) {
     return "The line is currently empty."
   } else {
-
+    for (katzDeli.length > 0)
   }
 }
